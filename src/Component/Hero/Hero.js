ce865eb6d26f86a91ff86c105253160bab74090d
David Moyano <davidmaximilianomoyano@gmail.com>
2023-04-10 12:14:34 -0300

    nueva pagina

---
+++ src/Component/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Hero = () => {
+    return (
+        <div className='hero-img hero-style' id='hero'>
+         
+        </div>
+    );
+};
+
+export default Hero;
